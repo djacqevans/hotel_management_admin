1cbf10ff16dfd7476e737ab4b225ebf36e6dc036
djacqevans <david@jacqevans.com>
2024-12-06 00:58:58 +0530

    db and env fixes

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -1,1 +1,9 @@
- 
+CREATE TABLE IF NOT EXISTS rooms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    room_type VARCHAR(50) NOT NULL,
+    floor INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
+    price_per_night DOUBLE PRECISION NOT NULL,
+    amenities JSONB NOT NULL
+);
